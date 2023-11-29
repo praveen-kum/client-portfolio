0f26f459388be85944b84cf12fc96336484459cb
praveen-kum <103064569+praveen-kum@users.noreply.github.com>
2023-11-29 12:13:58 +0530

    Update db.js
    removed db password

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 let mongoose =require("mongoose")
 
-let url = "mongodb+srv://praveen:hLrc1mjoTSQgAmRZ@cluster0.mooicr0.mongodb.net/employees"
+let url = "mongodb+srv://...:...@cluster0.mooicr0.mongodb.net/employees"
 mongoose.connect(url);
